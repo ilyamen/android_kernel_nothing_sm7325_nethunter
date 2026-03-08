001fa63ee500baaebb9ba8107a01f68808ff199d
Madara273 <ravenhoxs@gmail.com>
2026-03-08 18:46:05 +0000

    qcacld-3.0: Improved wlan_hdd_cfg80211_get_channel with monitor mode support
    Moved monitor mode channel reporting logic to the general wrapper of the wlan_hdd_cfg80211_get_channel function.
    Required for nl80211 (NL80211_CMD_GET_INTERFACE) and wext (SIOCGIWFREQ) so that tools like aireplay-ng / mdk3 can determine the current channel
    
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>
    Signed-off-by: Robin <hanya8493@gmail.com>

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_cfg80211.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_cfg80211.c
@@ -26086,20 +26086,77 @@ static int __wlan_hdd_cfg80211_get_channel(struct wiphy *wiphy,
 }
 
 /**
- * wlan_hdd_cfg80211_get_channel() - API to process cfg80211 get_channel request
+ * Station, SAP, etc - wlan_hdd_cfg80211_get_channel() - API to process cfg80211 get_channel request
  * @wiphy: Pointer to wiphy
  * @wdev: Pointer to wireless device
  * @chandef: Pointer to channel definition
  *
  * Return: 0 for success, non zero for failure
+ *
+ * Monitor - wlan_hdd_cfg80211_get_channel() - Report current operating channel
+ * @wiphy: wiphy handle
+ * @wdev: wireless_dev handle
+ * @chandef: output channel definition
+ *
+ * Required by nl80211 (NL80211_CMD_GET_INTERFACE) and wext (SIOCGIWFREQ)
+ * so that tools like aireplay-ng / mdk3 can determine the current channel.
+ *
+ * Return: 0 on success, -ENODATA if no channel is set.
  */
 static int wlan_hdd_cfg80211_get_channel(struct wiphy *wiphy,
 					 struct wireless_dev *wdev,
 					 struct cfg80211_chan_def *chandef)
 {
-	int errno;
+	struct hdd_adapter *adapter = WLAN_HDD_GET_PRIV_PTR(wdev->netdev);
+	struct hdd_station_ctx *sta_ctx;
+	struct hdd_mon_set_ch_info *ch_info;
+	struct ieee80211_channel *chan;
 	struct osif_vdev_sync *vdev_sync;
+	uint32_t freq;
+	int errno;
 
+	if (!adapter)
+		return -ENODATA;
+
+	/* -------- LOGIC FOR MONITOR MODE --------- */
+	if (adapter->device_mode == QDF_MONITOR_MODE) {
+		/* Primary source: mon_chan_freq */
+		freq = adapter->mon_chan_freq;
+
+		/* Fallback: station context ch_info */
+		if (!freq) {
+			sta_ctx = WLAN_HDD_GET_STATION_CTX_PTR(adapter);
+			ch_info = &sta_ctx->ch_info;
+			freq = ch_info->freq;
+		}
+
+		if (!freq)
+			return -ENODATA;
+
+		chan = ieee80211_get_channel(wiphy, freq);
+		if (!chan)
+			return -ENODATA;
+
+		cfg80211_chandef_create(chandef, chan, NL80211_CHAN_NO_HT);
+
+		/* Upgrade width if we know the bandwidth */
+		switch (adapter->mon_bandwidth) {
+		case CH_WIDTH_40MHZ:
+			chandef->width = NL80211_CHAN_WIDTH_40;
+			break;
+		case CH_WIDTH_80MHZ:
+			chandef->width = NL80211_CHAN_WIDTH_80;
+			break;
+		case CH_WIDTH_160MHZ:
+			chandef->width = NL80211_CHAN_WIDTH_160;
+			break;
+		default:
+			break;
+		}
+		return 0;
+	}
+
+	/* --- LOGIC FOR NORMAL MODES (Station, SAP, etc.) --- */
 	errno = osif_vdev_sync_op_start(wdev->netdev, &vdev_sync);
 	if (errno)
 		return errno;
